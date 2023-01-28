d19a6c463f9515b7b50e165f910d74a53bea80e2
Alex <ddragon99999@gmail.com>
2023-01-29 02:26:40 +0300

    with custom profile page

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {fetchFromAPI} from "../utils/fetchFromAPI";
+import {Box} from "@mui/material";
+import ChannelCard from "./ChannelCard";
+import Videos from "./Videos";
 
 const ChannelDetail = () => {
+    const  { id } = useParams()
+    const [channelDetail, setChannelDetail] = useState()
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
+
+            setChannelDetail(data?.items[0]);
+
+            const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+            setVideos(videosData?.items);
+        };
+
+        fetchResults();
+    }, [id]);
     return (
-        <div>
-            ChannelDetail
-        </div>
+        <Box minHeight={'95vh'}>
+            <Box>
+                <div style={{
+                    height:'300px',
+                    background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+                    zIndex: 10,
+                }}/>
+                <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
+            </Box>
+            <Box display={'flex'} p={2}>
+                <Box sx={{mr: {sm: '100px'}}}/>
+                    <Videos videos={videos}/>
+            </Box>
+        </Box>
     );
 };
 
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ import {CheckCircle} from "@mui/icons-material";
 const  VideoCard = ({video: {id: {videoId}, snippet}}) => {
     console.log(videoId, snippet)
     return (
-        <Card sx={{width: {md: '320px', xs: '100%'}}}>
+        <Card sx={{width: {md: '320px', xs: '100%'}, boxShadow: 'none', borderRadius: 0}}>
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
                 <CardMedia
                     image={snippet?.thumbnails?.high?.url}
