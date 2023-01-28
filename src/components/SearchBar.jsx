bb5bf85b48d7e686f815cca68b6c69b44f86860e
Alex <ddragon99999@gmail.com>
2023-01-29 02:41:55 +0300

    videos search added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IconButton, Paper} from "@mui/material";
 import {Search} from "@mui/icons-material";
-
+import { useNavigate } from "react-router-dom";
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const navigate = useNavigate()
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (searchTerm){
+            navigate(`/search/${searchTerm}`)
+            setSearchTerm('')
+        }
+    }
     return (
         <Paper
             component={'form'}
-            onSubmit={() => {}}
+            onSubmit={handleSubmit}
             sx={{
                 borderRadius: 20,
                 border: '1px solid #e3e3e3',
@@ -16,7 +26,7 @@ const SearchBar = () => {
             }}
         >
             <input className={'search-bar'} type="text" placeholder={'Search...'}
-            value={''} onChange={() => {}}/>
+            value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
             <IconButton type={'submit'} sx={{
                 p: '10px', color: 'red'
             }}>
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Box, Stack, Typography} from "@mui/material";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+import {fetchFromAPI} from "../utils/fetchFromAPI";
+import {useParams} from "react-router-dom";
 
 const SearchFeed = () => {
+    const [videos, setVideos] = useState([]);
+    const {searchTerm} = useParams()
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+            .then((data) => setVideos(data.items))
+    }, [searchTerm])
     return (
-        <div>
-            SearchFeed
-        </div>
+            <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+                <Typography variant={'h4'} fontWeight={'bold'} sx={{
+                    color: 'white'
+                }} mb={2}>
+                    Search Results for: <span style={{color: '#F31503'}}>
+                    {searchTerm}
+                </span> videos
+                </Typography>
+                <Videos videos={videos}/>
+            </Box>
     );
 };
 
