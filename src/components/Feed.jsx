002c640721bfc63e348791f6fef924c64517c873
Alex <ddragon99999@gmail.com>
2023-01-28 18:36:48 +0300

    main design added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {Box, Stack, Typography} from "@mui/material";
+import Sidebar from "./Sidebar";
+import Videos from "./Videos";
 
 const Feed = () => {
     return (
-        <div>
-            Feed
-        </div>
+        <Stack sx={{flexDirection: {sx: 'column', md: 'row'}}}>
+            <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight:
+            '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
+                <Sidebar/>
+                <Typography className={'copyright'} sx={{mt: 1.5,
+                color: 'white'}} variant={'body2'}>
+                    Copyright 2023 Yout 2.0
+                </Typography>
+            </Box>
+
+            <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+                <Typography variant={'h4'} fontWeight={'bold'} sx={{
+                    color: 'white'
+                }} mb={2}>
+                    New <span style={{color: '#F31503'}}>
+                    videos
+                </span>
+                </Typography>
+                <Videos videos={[]}/>
+            </Box>
+        </Stack>
     );
 };
 
