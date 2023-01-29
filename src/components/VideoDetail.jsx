04838f9aba76fd0601bca02e316a8d43173c50bf
Alex <ddragon99999@gmail.com>
2023-01-29 08:29:56 +0300

    react player added

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Box, Stack} from "@mui/material";
+import ReactPlayer from "react-player";
+import {useParams} from "react-router-dom";
+import {fetchFromAPI} from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
+    const [videoDetail, setVideoDetail] = useState(null)
+    const {id} = useParams()
+    useEffect(() => {
+        fetchFromAPI(`video?part=snippet,statistics&id-${id}`)
+            .then((data) => setVideoDetail(data.items[0]))
+    }, [id])
     return (
-        <div>
-            VideoDetail
-        </div>
+        <Box minHeight='95vh'>
+            <Stack direction={{xs: 'column', md: 'row'}}>
+                <Box flex={1}>
+                    <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+                        <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`}/>
+                    </Box>
+                </Box>
+            </Stack>
+        </Box>
     );
 };
 
